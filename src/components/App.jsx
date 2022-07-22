592316e006db7cd6843cbc7c36af13a8c912333c
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-07-22 09:28:29 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,47 @@
-// import { lazy } from "react";
-// import { Route, Routes } from "react-router-dom";
-// import { SharedLayout } from "./SharedLayout";
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-// const createAsyncComponent = (path) => lazy(() => import(path));
+const Home = lazy(() => import('pages/Home'));
+const MovieDetails = lazy(() => import('pages/MovieDetails'));
+const Movies = lazy(() => import('pages/Movies'));
+const CastList = lazy(() => import('./CastList/CastList'));
+const ReviwersList = lazy(() => import('./ReviwersList/ReviwersList'));
+const SharedLayout = lazy(() => import('./SharedLayout/SharedLayout'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
 
-// const About = createAsyncComponent("../pages/About");
-// const Home = createAsyncComponent("../pages/Home");
-// const ProductDetails = createAsyncComponent("../pages/ProductDetails");
-// const Products = createAsyncComponent("../pages/Products");
-// const NotFound = createAsyncComponent("../pages/NotFound")
+// import Home from 'pages/Home';
+// import MovieDetails from 'pages/MovieDetails';
+// import Movies from 'pages/Movies';
+// import NotFoundPage from 'pages/NotFoundPage';
 
-// const Mission = createAsyncComponent("./Mission");
-// const Team = createAsyncComponent("./Team");
-// const Reviews = createAsyncComponent("./Reviews");
+// import CastList from './CastList/CastList';
+// import ReviwersList from './ReviwersList/ReviwersList';
+// import SharedLayout from './SharedLayout/SharedLayout';
 
-
-
-
-import { Route, Routes } from "react-router-dom";
-import { SharedLayout } from "./SharedLayout";
-
-import About from "../pages/About";
-import Home from "../pages/Home";
-import Products from "../pages/Products";
-import NotFound from "../pages/NotFound";
-import ProductDetails from "../pages/ProductDetails";
-
-import Mission from "./Mission";
-import Team from "./Team";
-import Reviews from "./Reviews";
-
-export const App = () => {
+const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Home />} />
+    <>
+      <Routes>
 
-        <Route path="about" element={<About />}>
-          <Route path="mission" element={<Mission />} />
-          <Route path="team" element={<Team />} />
-          <Route path="reviews" element={<Reviews />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="movie" element={<Movies />} />
+
+          <Route path="movie/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<CastList />} />
+            <Route path="reviwers" element={<ReviwersList />} />
+          </Route>
+
+          <Route path="*" element={<NotFoundPage />} />
+          {/* <Route path="*" element={<Navigate to="/" />} /> */}
         </Route>
 
-        <Route path="products" element={<Products />} />
-        <Route path="products/:id" element={<ProductDetails />} />
-        <Route path="*" element={<NotFound />} />
-
-      </Route>
-    </Routes>
+      </Routes>
+      <ToastContainer autoClose={3000} theme="colored" pauseOnHover />
+    </>
   );
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
+export default App;
