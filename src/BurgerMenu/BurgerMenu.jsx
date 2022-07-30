6239a920684475a8ab2089c7c59c9a528c441a3e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-07-30 20:15:57 +0300

    Create Burger Menu

---
+++ src/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { elastic as Menu } from 'react-burger-menu';
+import { stylesMune } from './BurgerMunuStyles';
+
+const BurgerMenu = () => {
+  const [menuOpen, setmMenuOpen] = useState(false);
+
+  const showSettings = event => {
+    event.preventDefault();
+    setmMenuOpen(false);
+  };
+
+  const handleStateChange = state => {
+    setmMenuOpen(state.isOpen);
+  };
+
+  //   const disabledBurger = () => {
+  //     const screenWidth = window.screen.width;
+  //     const disabledBurgericon = screenWidth > 468 || menuOpen === true;
+  //     return disabledBurgericon ? false : null;
+  //   };
+
+  return (
+    <Menu
+      right
+      //   customBurgerIcon={disabledBurger()}
+      styles={stylesMune}
+      isOpen={menuOpen}
+      onStateChange={state => handleStateChange(state)}
+      pageWrapId={'page-wrap'}
+      outerContainerId={'outer-container'}
+    >
+      <p onClick={showSettings}>Home</p>
+      <p onClick={showSettings}>About</p>
+      <p onClick={showSettings}>Contact</p>
+      <p onClick={showSettings}>Settings</p>
+    </Menu>
+  );
+};
+
+export default BurgerMenu;
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -3,25 +3,31 @@ import { Box } from '../Box';
 import { Outlet } from 'react-router-dom';
 import { Header, SiteNavLink } from './SharedLayout.styled';
 import Container from 'components/Container';
-// import Loader from 'components/Loader/Loader';
-
+import BurgerMenu from 'BurgerMenu/BurgerMenu';
 
 const SharedLayout = () => {
   return (
-    <Container>
-      <Box display="grid" gridAutoColumns="1fr" gridGap="16px">
-        <Header>
-          <span>LOGO</span>
-          <nav>
-            <SiteNavLink to="/">Home</SiteNavLink>
-            <SiteNavLink to="/movie">Movie</SiteNavLink>
-          </nav>
-        </Header>
-        <Suspense fallback={null}>
-          <Outlet />
-        </Suspense>
-      </Box>
-    </Container>
+    <>
+      <div id="outer-container">
+        <BurgerMenu />
+        <div id="page-wrap">
+          <Container>
+            <Box display="grid" gridAutoColumns="1fr" gridGap="16px">
+              <Header>
+                <span>LOGO</span>
+                <nav>
+                  <SiteNavLink to="/">Home</SiteNavLink>
+                  <SiteNavLink to="/movie">Movie</SiteNavLink>
+                </nav>
+              </Header>
+              <Suspense fallback={null}>
+                <Outlet />
+              </Suspense>
+            </Box>
+          </Container>
+        </div>
+      </div>
+    </>
   );
 };
 
